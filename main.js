24d0a33bbc32dcf76438c8dad5c0fe9963175898
sorj-design <100023629+sorj-design@users.noreply.github.com>
2023-01-08 18:31:17 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+
+
+/* NAV MENU */
+
+const hamburger =document.querySelector('#favcon');
+const navMenu =document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', function(){
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+})
+
+
+
+
+/*   REGISTRATION FORM  */
+
+const form =document.forms.registration;
+const fullname =form.fullname;
+const email =form.email;
+const phone =form.phone;
+const register =document.getElementById('register')
+
+
+
+register.addEventListener('click', (e)=>{
+  e.preventDefault();
+  validation();
+  
+})
+
+
+function validation(){
+  
+
+  if(fullname.value ===''){
+  document.getElementById('error-name').innerText="Full name required";
+  document.getElementById('error-name').style.color='red';
+  document.getElementById('error-name').style.display='block';
+  document.getElementById('full-name').style.color='red';
+}
+
+else if(email.value ===''){
+  document.getElementById('error-email').innerText="Email Addres required";
+  document.getElementById('error-email').style.color='red';
+  document.getElementById('error-email').style.display='block';
+  document.getElementById('email-Address').style.color='red';
+}
+
+
+else if(phone.value ===''){
+  document.getElementById('error-phone').innerText="Phone Number required";
+  document.getElementById('error-phone').style.color='red';
+  document.getElementById('error-phone').style.display='block';
+  document.getElementById('phone-Number').style.color= 'red';
+}
+else{
+ 
+  console.log('Name' + fullname.value);
+  console.log('Email' + email.value);
+  console.log('Phone' + phone.value);
+  fullname.value ="";
+  email.value="";
+  phone.value="";
+}
+}
